d567824ac2732e4bc0c1a030a8766c254bbba012
Luis Felipe <luisfelipedasilva039@gmail.com>
2022-12-10 18:14:06 -0300

    tentando outra vez postar modificação

--- Teste2.py
+++ Teste2.py
@@ -8,3 +8,5 @@ print("""Olá mundo! testando modificação de arquivo commitado no github
       hahah que legal, muito divertido, androi app de angrboa, god of war ragnarok
       é muito bom e the last of us também, tanto a part 1 quanto a part 2 (a história
       da part 1 é melhor, sem querer desmerecer o 2 que também é muito lindo""")
+
+print('essa merda só da conflito pqp')
