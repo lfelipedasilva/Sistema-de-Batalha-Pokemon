f66585443ffe50427af477525481502637b4348f
Luis Felipe <luisfelipedasilva039@gmail.com>
2022-12-11 15:12:22 -0300

    jogo de adivinhação adicionado

--- Teste2.py
+++ Teste2.py
@@ -1,3 +1,5 @@
+from random import randint
+
 print('Hello, World!')
 
 a = int(input('Digite um número aleatório: '))
@@ -7,4 +9,13 @@ print(a)
 print("""Olá mundo! testando modificação de arquivo commitado no github
       hahah que legal, muito divertido, androi app de angrboa, god of war ragnarok
       é muito bom e the last of us também, tanto a part 1 quanto a part 2 (a história
-      da part 1 é melhor, sem querer desmerecer o 2 que também é muito lindo""")
+      da part 1 é melhor""")
+
+
+# vamos jogar um jogo ?
+
+while True:
+    comp = randint(1, 10)
+    jog = int(input('Tente adivinhar o numero de 1 á 10: '))
+    if jog == comp:
+        break
